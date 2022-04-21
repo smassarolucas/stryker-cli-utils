603779f13d422dd228cd6045103a31cfc2dbc36e
Lucas Smassaro <smassarolucas@gmail.com>
2022-04-21 20:23:22 -0300

    feat: parsing the app report into structs

--- helpers/filesystem.go
+++ helpers/filesystem.go
@@ -1,19 +1,22 @@
 package helpers
 
 import (
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
 )
 
 const (
 	strykerConfigNameSufix = "-stryker-config.json"
 	strykerConfigRegex     = ".*\\" + strykerConfigNameSufix + "$"
+	dirName                = "."
+	strykerReportsDirName  = "./StrykerOutput"
+	mutationReportRegex    = ".*\\-report.html$"
 )
 
 func GetStrykerConfigFileNames() []string {
-	dirName := "."
-
 	dir, err := os.Open(dirName)
 	if err != nil {
 		log.Fatalf("Couldn't open directory %v because of error %v", dirName, err.Error())
@@ -39,4 +42,30 @@ func GetStrykerConfigFileNames() []string {
 	}
 
 	return fileNames
+}
+
+func GetMutationReportsFilePaths() []string {
+	regex, err := regexp.Compile(mutationReportRegex)
+	if err != nil {
+		log.Fatalf("Couldn't compile the regex %v because of error %v", strykerConfigRegex, err.Error())
+	}
+
+	filePaths := []string{}
+	err = filepath.Walk(strykerReportsDirName, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if match := regex.MatchString(info.Name()); match {
+			filePaths = append(filePaths, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("Couldn't read filePaths for reports because of error %v", err.Error())
+	}
+
+	return filePaths
 }
--- helpers/stryker.go
+++ helpers/stryker.go
@@ -1,8 +1,14 @@
 package helpers
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
 const (
@@ -19,4 +25,95 @@ func RunStrykerMutator(configFile string) error {
 		return err
 	}
 	return nil
+}
+
+func MergeStrykerReports(filePaths []string) string {
+
+	// TODO: Make use of goroutines to parse files separatedly
+	for _, filePath := range filePaths {
+		fmt.Println(filePath)
+	}
+	return ""
+}
+
+type MutationReport struct {
+	SchemaVersion string `json:"schemaVersion"`
+	Thresholds    struct {
+		High int `json:"high"`
+		Low  int `json:"low"`
+	} `json:"thresholds"`
+	ProjectRoot string                        `json:"projectRoot"`
+	Files       map[string]MutationReportItem `json:"files"`
+}
+
+type MutationReportItem struct {
+	Language string `json:"language"`
+	Source   string `json:"source"`
+	Mutants  []struct {
+		ID          string `json:"id"`
+		MutatorName string `json:"mutatorName"`
+		Replacement string `json:"replacement"`
+		Location    struct {
+			Start struct {
+				Line   int `json:"line"`
+				Column int `json:"column"`
+			} `json:"start"`
+			End struct {
+				Line   int `json:"line"`
+				Column int `json:"column"`
+			} `json:"end"`
+		} `json:"location"`
+		Status string `json:"status"`
+		Static bool   `json:"static"`
+	} `json:"mutants"`
+}
+
+const (
+	reportDataPrefix      = "app.report = "
+	reportDataSuffix      = ";"
+	reportDataPrefixRegex = "^\\s*" + reportDataPrefix
+)
+
+func ParseMutationReport(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	regex, err := regexp.Compile(reportDataPrefixRegex)
+	if err != nil {
+		log.Fatalf("Couldn't compile the regex %v because of error %v", reportDataPrefixRegex, err.Error())
+	}
+
+	reader := bufio.NewReader(file)
+	line, err := readLine(reader)
+	mutationReport := MutationReport{}
+	for err == nil {
+		if match := regex.MatchString(line); match {
+			reportData := strings.Split(line, reportDataPrefix)[1]
+			reportData = strings.TrimSuffix(reportData, reportDataSuffix)
+			json.Unmarshal([]byte(reportData), &mutationReport)
+			// TODO: Alter the keys in the Files map to properly show the different projects
+
+			// TODO: Make this another method, running only once when the goroutines finish their processing
+			jsonReportData, _ := json.Marshal(mutationReport)
+			stringReportData := string(jsonReportData)
+			fmt.Println(string(stringReportData))
+		}
+		line, err = readLine(reader)
+	}
+}
+
+func readLine(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix              bool  = true
+		err                   error = nil
+		currentLine, fullLine []byte
+	)
+	for isPrefix && err == nil {
+		currentLine, isPrefix, err = r.ReadLine()
+		fullLine = append(fullLine, currentLine...)
+	}
+	return string(fullLine), err
 }
